855ca69a472da32cd739a359f2924cfd98bbceea
Eli West <70358327+xX-Eli-West-Xx@users.noreply.github.com>
2020-12-03 12:36:46 -0600

    save it

---
+++ allScript.js
@@ -0,0 +1,19 @@
+function supportbuttonleft() {
+    var x= document.getElementsByClassName("left-button");
+    x[0].innerHTML="Your order might not be finished yet. If your order isn't on it's way as soon as its finished, we don't have space currently. Be assured we are trying to get your order to you as soon as possible!";
+}
+
+function supportbuttonright() {
+    var w= document.getElementsByClassName("right-button");
+    w[0].innerHTML="This is the support button labeled 'Right'."
+}
+
+function supportbuttoncenter() {
+    var y= document.getElementsByClassName("center-button");
+    y[0].innerHTML="This is the support button labeled 'Center'."
+}
+
+function supportbuttonjustify() {
+    var z= document.getElementsByClassName("justify-button");
+    z[0].innerHTML="This is the support button labeled 'Justify'."
+}
