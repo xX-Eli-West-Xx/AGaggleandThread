6fb3b3e608d2db072979376f5f18cf4aaa451894
Eli West <70358327+xX-Eli-West-Xx@users.noreply.github.com>
2020-12-04 12:37:17 -0600

    did

--- allScript.js
+++ allScript.js
@@ -5,15 +5,19 @@ function supportbuttonleft() {
 
 function supportbuttonright() {
     var w= document.getElementsByClassName("right-button");
-    w[0].innerHTML="This is the support button labeled 'Right'."
+    w[0].innerHTML="There are the featured designes, but we also do monogramming if you want something personal."
 }
 
 function supportbuttoncenter() {
     var y= document.getElementsByClassName("center-button");
-    y[0].innerHTML="This is the support button labeled 'Center'."
+    y[0].innerHTML="If an item is out of stock, we are trying our best to get it back in stock. Check back in 4-5 business days for stock."
 }
 
 function supportbuttonjustify() {
     var z= document.getElementsByClassName("justify-button");
-    z[0].innerHTML="This is the support button labeled 'Justify'."
+    z[0].innerHTML="We are trying our hardest to get everyone's order done and shipped, so we may not be available right now."
+}
+function supportbuttonother() {
+    var v= document.getElementsByClassName("other-button");
+    v[0].innerHTML="If the question you want to ask isn't here, or you didn't get a clear answer, you can either use the phone number below, or email <i>gaggleandthread@gmail.com</i> to ask your question."
 }
